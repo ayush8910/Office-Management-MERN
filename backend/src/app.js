745601747d677fe7fda7b973296f01a0f7dc0be7
Ayush Gupta <102363120+ayush8910@users.noreply.github.com>
2025-11-17 16:07:28 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,8 +22,9 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
+  app.use((req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 }
 export default app;
