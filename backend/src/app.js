efea67d572e8a5e046887456c45b9f109afbf14e
Ayush Gupta <102363120+ayush8910@users.noreply.github.com>
2025-11-17 16:00:02 +0530

    Update app.js
    Serve frontend files in production environment.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import departmentRoutes from "./routes/departmentRoutes.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 
@@ -11,4 +14,16 @@ app.use(express.json());
 app.use("/api/departments", departmentRoutes);
 app.use("/api/employees", employeeRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 export default app;
